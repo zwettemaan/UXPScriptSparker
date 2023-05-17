ac0018cf7c677518a2d847aa4952aeccf29946af
Kris Coppieters <kris@rorohiko.com>
2023-05-18 08:15:58 +1200

    Reduce code duplication

--- Templates/$$~TARGETCONTEXT$$~InDesign ExtendScript/run.jsx
+++ Templates/$$~TARGETCONTEXT$$~InDesign ExtendScript/run.jsx
@@ -1,14 +1,1 @@
-﻿//@targetengine $$SHORTCODE$$.extendScriptEngine
-
-$include "infoheader.ijs"
-
-// Generic launcher. Running this script will run the script in $$DESPACED_TARGET_NAME$$.js 
-// in the ExtendScript environment
-
-if ("undefined" == typeof $$SHORTCODE$$) {
-    $$SHORTCODE$$ = {};
-}
-
-//@include "$$DESPACED_TARGET_NAME$$_helpers/jsx/runtime.jsx"
-$$SHORTCODE$$.loadModules($$SHORTCODE$$);
-
+﻿$include "run_as_ExtendScript.ijs"
---
+++ Templates/includes/run_as_ExtendScript.ijs
@@ -0,0 +1,13 @@
+//@targetengine $$SHORTCODE$$.extendScriptEngine
+
+$include "infoheader.ijs"
+
+// Generic launcher. Running this script will run the script in $$DESPACED_TARGET_NAME$$.js 
+// in the ExtendScript environment
+
+if ("undefined" == typeof $$SHORTCODE$$) {
+    $$SHORTCODE$$ = {};
+}
+
+//@include "$$DESPACED_TARGET_NAME$$_helpers/jsx/runtime.jsx"
+$$SHORTCODE$$.loadModules($$SHORTCODE$$);
